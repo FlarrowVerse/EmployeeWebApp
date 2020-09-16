bcf8e3f964928ee89c6455cf345aaeb6e7207259
FlarrowVerse <archisman1999@gmail.com>
2020-09-16 09:23:00 +0530

    First commit

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -9,28 +9,27 @@ import { Employee } from './employee';
 })
 export class EmployeeService {
 
-	private employeesUrl = '/employees';
-  private url = 'http://localhost:8000/api';
+	private url = 'https://employeewebappapi.herokuapp.com/api/employees';
 
 	httpOptions = {headers: new HttpHeaders({ 'Content-type': 'application/json' })};
 
 	constructor(private http: HttpClient) { }
 
 	getEmployees(): Observable<Employee[]> {
-		return this.http.get<Employee[]>(this.url + this.employeesUrl);
+		return this.http.get<Employee[]>(this.url);
 	}
 
 	updateEmployee(employee: Employee): Observable<Employee[]> {
-		this.http.put(this.url + this.employeesUrl + '/' + employee.id, employee, this.httpOptions).subscribe();
+		this.http.patch(this.url + '/' + employee.id, employee, this.httpOptions).subscribe();
     return this.getEmployees();
 	}
 
 	addEmployee(employee: Employee): Observable<Employee> {
-		return this.http.post<Employee>(this.url + this.employeesUrl, employee, this.httpOptions);
+		return this.http.post<Employee>(this.url, employee, this.httpOptions);
 	}
 
 	deleteEmployee(employee: number) {
-		const url = `${this.url+this.employeesUrl}/${employee}`;
-    this.http.delete(url, this.httpOptions).subscribe();
+		const url = `${this.url}/${employee}`;
+		this.http.delete(url, this.httpOptions).subscribe();
 	}
 }
